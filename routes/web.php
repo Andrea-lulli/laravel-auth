8974848adea06ac56ea1add4cd139290979bbc83
Andrea-Lulli <andrea.lulli89@gmail.com>
2023-01-24 16:56:01 +0100

    esercizio completo

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function(){
+        Route::get('/', 'HomeController@index')->name('index');
 });
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('{any?}', function(){
+    return view('guest.home');
+})->where("any", ".*");
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
